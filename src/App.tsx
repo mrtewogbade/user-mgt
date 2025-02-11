5d7cac26ef4a3749bc2d6ff30765d99ecdc6324b
David Hype <iamsocialhype@gmail.com>
2025-02-11 18:53:27 +0100

    fix/fix the tailwind not applying issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-
-
 function App() {
-
   return (
-
     <div>
-       <h1 className="text-3xl text-amber-300 font-bold underline">
-    Hello world!
-  </h1>
+      <h1 className="text-3xl text-amber-300 font-bold underline">
+        Hello world!
+      </h1>
+      <p className="bg-amber-800">HI</p>
     </div>
-
-   
-    
-  )
+  );
 }
 
-export default App
+export default App;
