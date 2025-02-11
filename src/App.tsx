1e885b2e13c0138a61e1712a945082d2d6efa557
mrtewogbade <johndharey@gmail.com>
2025-02-11 18:32:19 +0100

    set up

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  return (
+
+    <div>
+       <h1 className="text-3xl text-amber-300 font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+
+   
+    
+  )
+}
+
+export default App
